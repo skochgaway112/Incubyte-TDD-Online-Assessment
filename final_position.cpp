739dea3dc342bb8bf787f7d532f926ff16b16d83
Shreshtha Kochgaway <skochgaway112@gmail.com>
2023-08-16 13:44:03 +0530

    Test-5 change_in_direction individual testcases

--- final_position.cpp
+++ final_position.cpp
@@ -10,8 +10,12 @@ void no_change_in_direction(int x_direction, int y_direction, int z_direction,ch
 }
 
 
-void change_in_direction_only(char initial_direction){
+void change_in_direction_only(char initial_direction,char command){
   //here the direction will change with no increment or decrement
+  if(command=='r'){
+    if the initial direction is north , it will rotate 90 degrees to the right , else if south then again 90 degrees to the right to West
+    and if it is in west then it will move to north and if in east then it will move towards south
+  }
 } 
 
 void directions(vector<char>&commands,int x_direction, int y_direction , int z_direction, char initial_direction){
@@ -19,7 +23,7 @@ void directions(vector<char>&commands,int x_direction, int y_direction , int z_d
          if(commands[i]=='f' || commands[i]=='b'){
             no_change_in_direction(x_direction,y_direction,z_direction,commands[i], initial_direction);
          }else{
-            change_in_direction_only(inital_direction);
+            change_in_direction_only(inital_direction,command[i]);
          }
     }
 }     
