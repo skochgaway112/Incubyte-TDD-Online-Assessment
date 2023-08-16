925d2cb6e26d473ab40b6d222ac977be3b1ff74b
Shreshtha Kochgaway <skochgaway112@gmail.com>
2023-08-16 13:39:30 +0530

    Production level code for no_change_in_direction

--- final_position.cpp
+++ final_position.cpp
@@ -1,19 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void no_change_in_direction(int x_direction, int y_direction, int z_direction){
-  here only the indexes will be incremented or decremented  
+void no_change_in_direction(int x_direction, int y_direction, int z_direction,char command,char intial_direction){
+  if(command=='f'){
+    y_direction++;
+  }else if(command=='b'){
+    y_direction--;
+  }
 }
 
 
 void change_in_direction_only(char initial_direction){
-  here the direction will change with no increment or decrement
+  //here the direction will change with no increment or decrement
 } 
 
 void directions(vector<char>&commands,int x_direction, int y_direction , int z_direction, char initial_direction){
     for(int i=0;i<commands.size();i++){
          if(commands[i]=='f' || commands[i]=='b'){
-            no_change_in_direction(x_direction,y_direction,z_direction);
+            no_change_in_direction(x_direction,y_direction,z_direction,commands[i], initial_direction);
          }else{
             change_in_direction_only(inital_direction);
          }
