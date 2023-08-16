2c9cacba420809d2dbf408130921c480a52a5fc4
Shreshtha Kochgaway <skochgaway112@gmail.com>
2023-08-16 10:55:07 +0530

    Test-1 Compilation error with first given inputs

--- final_position.cpp
+++ final_position.cpp
@@ -1,5 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+ 
+     
+   Given{
+    intial_direction_in_x;
+    initial_direction_in_y;
+    initial_direction_in_z;
+   }
+
     return 0;
 }
