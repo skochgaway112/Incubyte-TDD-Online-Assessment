2355565f09f5987e4fcc31798f9654f770a2b258
Shreshtha Kochgaway <skochgaway112@gmail.com>
2023-08-16 11:00:05 +0530

    Production level code for Test-1

--- final_position.cpp
+++ final_position.cpp
@@ -3,11 +3,22 @@ using namespace std;
 int main(){
  
      
-   Given{
-    intial_direction_in_x;
-    initial_direction_in_y;
-    initial_direction_in_z;
-   }
+   int initial_direction_x;
+   cin>>initial_direction_x;
+
+
+
+   int initial_direction_y;
+   cin>>initial_direction_y;
+
+   int initial_direction_z;
+   cin>>initial_direction_z;
+
+   //show the input taken(for verification)
+
+   cout<<initial_direction_x<<" "<<initial_direction_y<<" "<<initial_direction_z<<endl;
+
+
 
     return 0;
 }
