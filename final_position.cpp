1fc3c1d31493daa248258b3b4da92a63f84cf305
Shreshtha Kochgaway <skochgaway112@gmail.com>
2023-08-16 11:44:47 +0530

    Test-3 Given cases of motion and rotation

--- final_position.cpp
+++ final_position.cpp
@@ -17,7 +17,12 @@ int main(){
      vector<char>commands;
      commands={'f','r','u','b','l'};
       
-  
+  Given{
+    initial position and direction is (0,0,0) and N
+    if(command is f or b)- The spacecraft would move one step forward and backward depending on the current direction 
+    if(command is l or r)- the spacecraft will turn 90 degrees to left or right, changing the direction
+    if(command is u or d)- the spacecraft will again turn the direction rotating upwards or downwards 
+  }
 
     return 0;
 }
